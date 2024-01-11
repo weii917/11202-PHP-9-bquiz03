f10ced86d58faafc4ca2b8a2b4f92c0e58ed6faf
wei <lin951016@gmail.com>
2024-01-11 15:31:31 +0800

    完成往上往下

--- back/poster.php
+++ back/poster.php
@@ -37,11 +37,11 @@
                         <input type="text" name="name[]" value="<?= $po['name']; ?>">
                     </div>
                     <div>
-                        <input type="button" value="往上" 
+                        <input class="btn" type="button" value="往上" 
                           date-id="<?=$po['id'];?>"
                           data-sw="<?=($idx!=0)?$pos[$idx-1]['id']:$po['id'];?>">
-                          
-                        <input type="button" value="往下" 
+
+                        <input class="btn" type="button" value="往下" 
                           date-id="<?=$po['id'];?>" 
                           data-sw="<?=((count($pos)-1)!=$idx)?$pos[$idx+1]['id']:$po['id'];?>">
                     </div>
@@ -84,3 +84,15 @@
         <div class="ct"><input type="submit" value="新增"><input type="reset" value="重置"></div>
     </form>
 </div>
+
+
+<script>
+$(".btn").on("click",function(){
+    let id=$(this).data('id');
+    let sw=$(this).data('sw');
+    let table='poster'
+    $.post("./api/sw.php",{id,sw,table},()=>{
+        location.reload()
+    })
+})
+</script>
