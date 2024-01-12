21ab1a6519c13e9669b2d5564cf18d45c845ff1a
wei <lin951016@gmail.com>
2024-01-12 16:40:01 +0800

    撰寫輪播測試程式

--- front/main.php
+++ front/main.php
@@ -108,6 +108,23 @@
 </div>
 <script>
     $(".item").eq(0).show();
+    
+    let now = 0;
+    let timer = setInterval(() => {
+        slide()
+    }, 3000)
+
+    function slide() {
+        $(".item").hide();
+        now++;
+        if (now > 8) {
+            now = 0;
+        }
+        $(".item").eq(now).show();
+    }
+
+
+
     let total = $(".btn").length
     let p = 0;
     console.log(total)
