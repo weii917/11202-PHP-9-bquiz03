3739689bd83d440e7b5e6f66ce441b7ee4947d1b
wei <lin951016@gmail.com>
2024-01-15 09:30:56 +0800

    完成預告片自動輪播轉場功能

--- front/main.php
+++ front/main.php
@@ -1,7 +1,10 @@
 <style>
     .lists {
-        /*  position: relative; */
+        position: relative;
         left: 114px;
+        width: 200px;
+        height: 240px;
+        overflow: hidden;
     }
 
     .item * {
@@ -14,6 +17,7 @@
         margin: auto;
         box-sizing: border-box;
         display: none;
+        position: absolute;
     }
 
     .item div img {
@@ -79,7 +83,7 @@
             $posters = $Poster->all(['sh' => 1], " order by rank");
             foreach ($posters as $idx => $poster) {
             ?>
-                <div class="item">
+                <div class="item" data-ani="<?= $poster['ani']; ?>">
                     <div><img src="./img/<?= $poster['img']; ?>" alt=""></div>
                     <div><?= $poster['name']; ?></div>
                 </div>
@@ -108,24 +112,40 @@
 </div>
 <script>
     $(".item").eq(0).show();
-    
+    let total = $(".btn").length
     let now = 0;
     let timer = setInterval(() => {
         slide()
     }, 3000)
 
     function slide() {
-        $(".item").hide();
-        now++;
-        if (now > 8) {
-            now = 0;
+        let ani = $(".item").eq(now).data("ani");
+        let next = now + 1;
+        if (next >= total) {
+            next = 0;
         }
-        $(".item").eq(now).show();
+        switch (ani) {
+            case 1:
+                $(".item").eq(now).fadeOut(1000, function() {
+                    $(".item").eq(next).fadeIn(1000);
+                });
+                break;
+            case 2:
+                $(".item").eq(now).hide(1000, function() {
+                    $(".item").eq(next).show(1000);
+                });
+                break;
+            case 3:
+                $(".item").eq(now).slideUp(1000, function() {
+                    $(".item").eq(next).slideDown(1000);
+                });
+                break;
+        }
+        now = next;
     }
 
 
 
-    let total = $(".btn").length
     let p = 0;
     console.log(total)
     $(".left,.right").on("click", function() {
