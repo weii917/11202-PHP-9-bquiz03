6eab77d6a0b34479a5b222fc5dbd62313fff30eb
wei <lin951016@gmail.com>
2024-01-19 10:03:30 +0800

    建立劃位畫面中的座位表,並計算排號

--- api/booking.php
+++ api/booking.php
@@ -1,27 +1,55 @@
 <?php
 include_once "db.php";
-$movie = $Movie->find($_GET['movie_id']);
-$date = $_GET['date'];
-$session = $_GET['session'];
+$movie=$Movie->find($_GET['movie_id']);
+$date=$_GET['date'];
+$session=$_GET['session'];
 ?>
 <style>
-    #room {
-        background-image: url('./icon/03D04.png');
-        background-position: center;
-        background-repeat: none;
-        width: 540px;
-        height: 370px;
-        margin: auto;
-    }
+ #room{
+    background-image: url('./icon/03D04.png');
+    background-position: center;
+    background-repeat: none;
+    width:540px;
+    height:370px;
+    margin:auto;
+    box-sizing: border-box;
+    padding:19px 112px 0 112px;
+    
+ }
+ .seat {
+    width: 63px;
+    height: 85px;
+}
+
+.seats {
+    display: flex;
+    flex-wrap: wrap;
+}   
 </style>
 
-<div id="room"></div>
+<div id="room">
+<div class="seats">
+    <?php
+    for($i=0;$i<20;$i++){
+
+        echo "<div class='seat'>";
+        echo "<div class='ct'>";
+        echo (floor($i/5)+1) . "排";
+        echo (($i%5)+1) . "號";
+        echo "</div>";
+        
+        echo "</div>";
+    }
+    ?>
+</div>
+
+</div>
 <div id="info">
-    <div>您選擇的電影是：<?= $movie['name']; ?></div>
-    <div>您選擇的時刻是：<?= $date; ?> <?= $session; ?></div>
-    <div>您已經勾選<span id='tickets'>0</span>張票，最多可以購買四張票</div>
-    <div>
-        <button onclick="$('#select').show();$('#booking').hide()">上一步</button>
-        <button>訂購</button>
-    </div>
+<div>您選擇的電影是：<?=$movie['name'];?></div>
+<div>您選擇的時刻是：<?=$date;?> <?=$session;?></div>
+<div>您已經勾選<span id='tickets'>0</span>張票，最多可以購買四張票</div>
+<div>
+    <button onclick="$('#select').show();$('#booking').hide()">上一步</button>
+    <button>訂購</button>
+</div>
 </div>
